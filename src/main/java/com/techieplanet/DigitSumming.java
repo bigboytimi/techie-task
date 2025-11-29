fa25fb5c99b5f6fac940a93b8d3c1d16bd87730d
timiolowookere <timiolowookere79@gmail.com>
2025-11-29 19:03:12 +0100

    ConvertNumeralsToWords

---
+++ src/main/java/com/techieplanet/DigitSumming.java
@@ -0,0 +1,20 @@
+package com.techieplanet;
+
+/**
+ * @author timiolowookere
+ * @since 29-11-2025
+ */
+public class DigitSumming {
+    // Part A logic reused
+    public static int sumDigits(String s) {
+        if (s.length() == 0) return 0;
+        return (s.charAt(0) - '0') + sumDigits(s.substring(1));
+    }
+
+    // Part B - Recursive digital root
+    public static int digitalRoot(String s) {
+        int sum = sumDigits(s);
+        if (sum < 10) return sum;
+        return digitalRoot(String.valueOf(sum));
+    }
+}
---
+++ src/main/java/com/techieplanet/DuplicateRemover.java
@@ -0,0 +1,34 @@
+package com.techieplanet;
+
+/**
+ * @author timiolowookere
+ * @since 29-11-2025
+ */
+public class DuplicateRemover {
+    public static int[][] removeDuplicates(int[][] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            // Find maximum value in row to size visited array efficiently
+            int maxVal = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > maxVal) {
+                    maxVal = arr[i][j];
+                }
+            }
+
+            int[] visited = new int[maxVal + 1];
+
+            for (int j = 0; j < arr[i].length; j++) {
+                int val = arr[i][j];
+                if (visited[val] == 0) {
+                    visited[val] = 1; // first appearance
+                } else {
+                    arr[i][j] = 0; // duplicate → replace with 0
+                }
+            }
+        }
+
+        return arr;
+    }
+}
